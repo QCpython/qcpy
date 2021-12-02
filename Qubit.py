614883a496fd9655f6971005a56f7fa35f7e6eab
parisosuch-dev <paris.osuch@gmail.com>
2021-12-01 20:30:14 -0800

    changed to complex data type

--- Qubit.py
+++ Qubit.py
@@ -9,7 +9,7 @@ class Qubit:
         self.state = None # state of the qubit
         # create 2x1 matrix for state
         if(state == 0):
-            self.state = [[1], [0]]
+            self.state = [[1+0j], [0+0j]]
         if(state == 1):
-            self.state = [[0], [1]]
+            self.state = [[0+0j], [1+0j]]
         
