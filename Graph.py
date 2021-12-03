2b0d506df838f9ce64ef744e87a561dfea58590e
123freezebrennan <freezebrennan@gmail.com>
2021-12-03 13:55:12 -0800

    Changed output of graph to be stored into a .png

--- Graph.py
+++ Graph.py
@@ -1,20 +1,31 @@
 import matplotlib.pyplot as plt
+from matplotlib.pyplot import figure
 import numpy as np
 import math
 
+"""
+Graph.py
+Creates a .png  matplotlib graph of the called in array from QauntumCircuit.py class method "graph(self)"
+"""
 
 class Graph:
-    #public
     def __init__(self, qubits):
-        """
-        """
+        self._qubitValues = []
+        self._percents = []
+        self._qubits = []
         self._size = len(qubits)
         self._qubits = qubits
+
         self.getQubitValues()
         self.getPercents()
 
     def getQubitValues(self):
         """
+        Get Qubit Values to Turn Into String of Values for x axis.
+        Params:
+            None
+        Returns:
+            None
         """
         if (self._size % 2 != 0):
             print("QCpy: A critical error has occured and the occuring size of final qubit is: " + str(self._size))
@@ -22,47 +33,45 @@ class Graph:
         for i in range(self._size):
             placeBin = str(bin(i))
             placeBin = placeBin[2:]
-            placeBin = (round((math.log(self._size)) + 1 - len(placeBin))*'0') + placeBin
+            placeBin = (round((math.log(self._size)) + 1 - len(placeBin)) * '0') + placeBin
             self._qubitValues.append(placeBin)      
 
     def getPercents(self):
         """
+        Get percents in from the called in array in the constructor and compares all the valeus that they are equal to one.
+        Params:
+            None
+        Returns:
+            None
         """
         total = 0
         for i in range(self._size):
-            self._percents.append(100 * self._qubits[i][0])
-            total += self._qubits[i][0]
+            if (self._qubits[i][0] == 1/math.sqrt(2)):
+                self.percents.append(100 * .5)
+            else:
+                self._percents.append(100 * self._qubits[i][0])
+                total += self._qubits[i][0]
         if 1 < total:
             print("QCpy: A critical error has occured and the calculation is above 100%")
             exit(1)
         
     def makeGraph(self):
         """
+        Makes bar graph and creates an image from above class methods.
+        Params:
+            None
+        Returns:
+            None
         """
-        graph = plt.figure()
+        plt.figure(figsize=(self._size,5)) 
         ax = plt.axes()
+        ax.bar(self._qubitValues, self._percents, align='center', color='#34b4fc')
         ax.set_ylim(0, 100)
-        ax.bar(self._qubitValues, self._percents)
+        ax.grid(True)
+        ax.xaxis.grid()
         plt.xlabel('Computational basis states')
         plt.ylabel('Probability (%)')
         plt.title('Probabilities')
-        plt.show()
-    #private
-    #is the size of the array which will be then converted to the qubit values
-    _size = 0
-    #array of strings to determine the value of the qubit based off of size
-    _qubitValues = []
-    #percantages
-    _percents = []
-    # called in qubit values
-    _qubits = []
-
-
-def main():
-    test = Graph([ [.25], [0], [0], [.25], [0], [0], [0], [0], [.0], [.25], [.25], [0], [0], [0], [0], [0] ])
-    test.makeGraph()
-
-if __name__ == "__main__":
-    main()
+        plt.savefig('probability.png')
 
     
