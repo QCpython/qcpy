53568aa7425b72fdce5718537582fa940319d8f3
aundre-barras <barras@sonoma.edu>
2022-11-08 23:42:29 -0800

    added example files for QSphere and StateVector (needs documentation)

---
+++ examples/QSphere.py
@@ -0,0 +1,13 @@
+from QuantumCircuit import *
+from Visualizer import *
+
+# creates a circuitfor 3 qubits
+qc = QuantumCircuit(3)
+# call the hadamard gate on all 3 qubits
+qc.hadamard(0)
+qc.hadamard(1)
+qc.hadamard(2)
+# call the QSphere class on the circuit
+sphere_test = QSphere(qc)
+# call the makeSphere method, this will show the sphere in a new window
+sphere_test.makeSphere(save=False, show=True)
---
+++ examples/StateVector.py
@@ -0,0 +1,24 @@
+from QuantumCircuit import *
+from Visualizer import *
+
+circuit = QuantumCircuit(5, little_endian = True)
+circuit.hadamard(0)
+circuit.hadamard(1)
+circuit.hadamard(2)
+circuit.hadamard(3)
+circuit.hadamard(4)
+
+circuit.cnot(0,1)
+circuit.toffoli(0,1,2)
+circuit.t(1)
+circuit.t(2)
+circuit.hadamard(1)
+circuit.hadamard(2)
+circuit.z(3)
+circuit.toffoli(0, 2, 1)
+circuit.sdg(2)
+circuit.u(2, np.pi / 2)
+circuit.rz(4)
+
+test = StateVector(circuit)
+test.makeGraph(save=False, show=True, darkmode=True)
