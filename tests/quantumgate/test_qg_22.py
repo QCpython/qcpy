64a68a5e44d0904bb6793db2cd6a4a5de270c7db
123freezebrennan <freezebrennan@gmail.com>
2023-01-29 18:05:53 -0800

    Added more tests to confirm gates just in case anything is dismanteled. Will be used for pushing code to github repo

---
+++ tests/quantumgate/test_qg_22.py
@@ -0,0 +1,10 @@
+from QCpy.QuantumGate import Cr
+import numpy as np
+
+def test_qg_22():
+    assert(Cr().matrix == np.array([
+            [1+0j, 0+0j, 0+0j, 0+0j],
+            [0+0j, 1+0j, 0+0j, 0+0j],
+            [0+0j, 0+0j, 1+0j, 0+0j],
+            [0+0j, 0+0j, 0+0j, np.exp((np.pi / 2) * 0+1j)]
+        ], 'F')).all(), 'test_qg_22 Failed'
