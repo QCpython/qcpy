38adcddb8d0485bef7ff8902206d74d5462808f3
Brennan Freeze <freezebrennan@gmail.com>
2023-04-30 14:23:42 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -13,17 +13,12 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/QCpython/QCpy",
     packages=setuptools.find_packages(),
-    classifiers=[
-        "Development Status :: 3 - Alpha",
-        "License MIT",
+    classifiers = [
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
+        "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires=">=3.6",
+    python_requires=">=3.8",
     install_requires=[
         "numpy>=1.22.0",
         "matplotlib>=3.5.1",
