4c424d82fc06d2fb7fa3011485fb8cc2b732d510
123freezebrennan <freezebrennan@gmail.com>
2023-09-12 16:36:35 -0700

    ready for publish

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="qcpython",
-    version="1.1.0",
+    version="1.1.1",
     author='Brennan Freeze, Paris Osuch, Aundre Barras, Soren Richenberg, Suzanne Rivoire',
     author_email='freezebrennan@gmail.com, osuch@sonoma.edu, barras@sonoma.edu, richenbe@sonoma.edu, rivoire@sonoma.edu',
     description="QCpy is an open source python library and collaborative project for flexible simulations and visualizations of quantum circuits. Designed by college students with students in mind, this library contains a powerful set of tools to teach computer scientists about the emerging discipline of quantum computing.",
@@ -20,8 +20,10 @@ setuptools.setup(
     ],
     python_requires=">=3.8",
     install_requires=[
-        "matplotlib==3.7.2",
-        "numpy==1.25.1",
-        "setuptools==65.5.0",
+    "matplotlib==3.7.2",
+    "numpy==1.25.1",
+    "setuptools==65.5.0",
+    "scipy==1.11.2",
+    "pytest==7.4.2",
     ],
 )
