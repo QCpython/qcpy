c17bbdc850725dfd0fa4b3ccebfc90fe99d49234
paris osuch <paris.osuch@gmail.com>
2024-09-13 22:35:40 -0700

    remove unused variable

--- src/quantum_circuit/quantum_circuit.py
+++ src/quantum_circuit/quantum_circuit.py
@@ -36,7 +36,6 @@ class QuantumCircuit:
 
         elif self.gpu:
             try:
-                check = subprocess.check_output(["nvcc", "--version"]).decode()
                 self.calculator = GpuCalculator(qubits, big_endian, prep)
             except FileNotFoundError:
                 print("ERROR: CUDA NOT INSTALLED. SWITCHING TO BASE...")
