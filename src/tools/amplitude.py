a06cc6769858248402187b3d14b600b1b6c9367b
123freezebrennan <freezebrennan@gmail.com>
2024-10-27 17:42:08 -0700

    Formatted and added error classes tools.amplitude

--- src/tools/amplitude.py
+++ src/tools/amplitude.py
@@ -1,18 +1,55 @@
-from numpy import arcsin, around, power, sqrt, log2
+from numpy import arcsin, around, power, sqrt, log2, ndarray
+from numpy.typing import NDArray
 from .base import convert_state
+from typing import Union
+from ..quantum_circuit import QuantumCircuit
+from ..errors import *
 
 
-def amplitude(quantumstate, show_bit=-1, round: int = 3, radian: bool = False):
-    quantumstate = convert_state(quantumstate)
-    ToolsException().test_amplitude(show_bit, round, amplitude)
+def amplitude(
+    quantumstate: Union[ndarray, QuantumCircuit],
+    show_bit: Union[int, str] = -1,
+    round: int = 3,
+    radian: bool = False,
+) -> NDArray:
+    """Outputs the amplitude of a quantum circuit state.
+    ```
+    from qcpy import quantumcircuit, amplitude
+    amplitude(quantumcircuit(qubits = 2)
+    ```
+    Args:
+        state: (ndarray/quantumcircuit, required): State to convert to a amplitude array. No default.
+        show_bit: (str/int, optional): Can specify showing a single value from the state. Defaults to -1.
+        round: (int, optional): Rounds the amplitude array by a given value. Defaults to 3.
+        radian: (bool, true): Can represent the amplitude array in radians or not. Defaults to true.
+    Returns:
+        NDArray: Amplitude array from given state.
+    """
+    state = convert_state(quantumstate)
+    size = int(log2(state.size))
+
+    if round < 0:
+        RoundBelowZero(f"Cannot round to {round} needs to be 0 or greater")
+
     if isinstance(show_bit, int) and show_bit < 0:
-        amplitude = sqrt(power(quantumstate.real, 2) + power(quantumstate.imag, 2))
+        amplitude = sqrt(power(state.real, 2) + power(state.imag, 2))
 
-    elif isinstance(show_bit, str) or isinstance(show_bit, int):
+    elif isinstace(show_bit, str):
+        amplitude = int(show_bit, 2)
+        if 2**size <= amplitude:
+            OutOfRangeError(
+                f"Cannot show bit of value {show_bit} as it is out of range"
+            )
+        amplitude = sqrt(
+            power(state[amplitude].real, 2) + power(state[amplitude].imag, 2)
+        )
 
+    elif isinstance(show_bit, int):
         amplitude = show_bit
-        if isinstance(show_bit, str):
-            amplitude = int(show_bit, 2)
+        if 2**size <= amplitude:
+            OutOfRangeError(
+                f"Cannot show bit of value {show_bit} as it is out of range"
+            )
         amplitude = sqrt(
             power(quantumstate[amplitude].real, 2)
             + power(quantumstate[amplitude].imag, 2)
