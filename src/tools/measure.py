b0ff7a9175b8782aebb747fda9f050b62170081c
123freezebrennan <freezebrennan@gmail.com>
2024-10-27 17:59:47 -0700

    Formatted measure, may need code coverage

--- src/tools/measure.py
+++ src/tools/measure.py
@@ -1,10 +1,22 @@
-from numpy import arange, random, log2
+from numpy import arange, random, log2, ndarray
+from numpy.typing import NDArray
+from typing import union
 from .probability import probability
 from .base import convert_state
+from ..errors import *
+from ..quantum_circuit import QuantumCircuit
 
 
-def measure(state):
-    state = convert_state(state)
+def measure(quantumstate: Union[QuantumCircuit, ndarray]) -> NDArray:
+    """Outputs the measure of a quantum circuit state.
+    ```
+    from qcpy import quantumcircuit, measure
+    measure(quantumcircuit(qubits = 2)
+    ```
+    Returns:
+        NDArray: Amplitude array from given state.
+    """
+    state = convert_state(quantumstate)
     size = int(log2(state.size))
     probabilities = probability(state, round=size)
     final_state = random.choice(arange(len(state), dtype=int), 1, p=probabilities)
