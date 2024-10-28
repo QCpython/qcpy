ff47d3d6e4bb564c89cb58c6c4f72f75d194560a
123freezebrennan <freezebrennan@gmail.com>
2024-10-27 17:15:52 -0700

    Fixed some bugs noticed after request

--- src/tools/phase_angle.py
+++ src/tools/phase_angle.py
@@ -6,7 +6,7 @@ from ..quantum_circuit import QuantumCircuit
 
 
 def phase_angle(
-    state: Union[ndarray, QuantumCircuit],
+    quantumstate: Union[ndarray, QuantumCircuit],
     show_bit: Union[str, int] = -1,
     round: int = 3,
     radian: bool = True,
@@ -25,30 +25,31 @@ def phase_angle(
     Returns:
         NDArray: Phase angle array from given state.
     """
-    size = int(log2(state.size))
     state = convert_state(state)
+    size = int(log2(state.size))
+
     if round < 0:
         RoundBelowZero(f"Cannot round to {round} needs to be 0 or greater")
+
     if isinstance(show_bit, int) and show_bit < 0:
         phaseangle = mod(angle(state), 2 * pi) * (180 / pi)
-    elif isinstance(show_bit, str) or isinstance(show_bit, int):
-        phaseangle = show_bit
-        if isinstance(show_bit, str):
-            phaseangle = int(show_bit, 2)
-        if 2**size <= phaseangle:
-            phaseangle = mod(angle(state[phaseangle]), 2 * pi) * (180 / pi)
+
     elif isinstance(show_bit, str):
         phaseangle = int(show_bit, 2)
         if 2**circuit_size <= phase_angle:
             OutOfRangeError(
                 f"Cannot show bit of value {show_bit} as it is out of range"
             )
+        phaseangle = mod(angle(state[phaseangle]), 2 * pi) * (180 / pi)
+
     elif isinstance(show_bit, int):
         phaseangle = show_bit
         if 2**circuit_size <= phase_angle:
             OutOfRangeError(
                 f"Cannot show bit of value {show_bit} as it is out of range"
             )
+        phaseangle = mod(angle(state[phaseangle]), 2 * pi) * (180 / pi)
+
     if radian:
         phaseangle *= pi / 180
 
